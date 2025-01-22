24bbc1f1343383658761bdc71b6c7891fd759cad
Aleena-Siby <aleenasiby92002@gmail.com>
2025-01-22 12:50:55 +0530

    revised

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -116,7 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # This is where your static files are located during development
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # This is the directory where static files will be collected
+
 
 
 
